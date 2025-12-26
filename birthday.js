5922c8e9f54b2b0dc7dc65213ba28f86b695ff5f
2176644349-star <2176644349@qq.com>
2025-12-26 18:20:14 +0800

    Update birthday.js

--- birthday.js
+++ birthday.js
@@ -71,9 +71,9 @@ document.addEventListener('DOMContentLoaded', function() {
     createConfetti();
     
     // If user didn't go through login page, redirect to login
-    if (!localStorage.getItem('birthdayUser')) {
-        window.location.href = '../index.html';
-    }
+    //if (!localStorage.getItem('birthdayUser')) {
+        //window.location.href = '../index.html';
+    //}
 });
 
 // Function to create confetti
